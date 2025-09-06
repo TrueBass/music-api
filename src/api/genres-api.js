35619171e6ee23a4790fb43f7b3e4ba3d04f0cc4
TrueBass <ttrrbb81@gmail.com>
2025-09-06 23:22:16 +0200

    added genres api calls

--- src/api/genres-api.js
+++ src/api/genres-api.js
@@ -24,3 +24,54 @@ export const getAllGenres = async () => {
         return null;
     }
 };
+
+export const getGenre = async (genreId) => {
+    let bearerToken = localStorage.getItem("accessToken");
+
+    try {
+        const response = await fetch(`${GENRE_API_URL}?genreId=${genreId}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${bearerToken}`
+            }
+        });
+        const deserealizedRes = await response.json();
+
+        if(!response.ok) {
+            
+            console.log(deserealizedRes);
+            return null;
+        }
+        return deserealizedRes;
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
+};
+
+export const addGenre = async (genreName) => {
+    let bearerToken = localStorage.getItem("accessToken");
+
+    try {
+        const response = await fetch(`${GENRE_API_URL}/`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${bearerToken}`
+            },
+            body: genreName
+        });
+        const deserealizedRes = await response.json();
+
+        if(!response.ok) {
+            
+            console.log(deserealizedRes);
+            return null;
+        }
+        return deserealizedRes;
+    } catch (error) {
+        console.log(error.message);
+        return null;
+    }
+};
